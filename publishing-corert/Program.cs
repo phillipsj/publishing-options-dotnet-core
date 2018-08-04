ed73d81b19b6d88cb9fc0adcbdc74db5f27003e0
Charles Phillips <cphill11@gmail.com>
2018-08-03 21:19:42 -0400

    Initial commit.

---
+++ publishing-corert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace publishing_corert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello {args[0]}!");
+        }
+    }
+}
---
+++ sample-application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample_application
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine($"Hello {args[0]}!");
+    }
+  }
+}
